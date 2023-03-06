5d7da056c6a5af92ab6e1277bed7049e5920be6a
Dh A Nu <82042269+mr-robot469@users.noreply.github.com>
2023-03-06 23:38:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 //your code here
+let pageNumberHere = 1;
+let btn_prev = document.getElementById("load_prev")
+let btn_Next = document.getElementById("load_next")
+let list     = document.getElementById("list");
+
+async function getData(){
+	const response = await fetch(`https://api.github.com/repositories/1296269/issues?page=${pageNumberHere}&per_page=5`);
+    const data = await response.json();
+    console.log(data);
+    let users = " "
+    data.map((item)=>{
+         users+= `<li>${item.url}</li>`
+    })
+    list.innerHTML = users;
+
+    if (pageNumberHere == 1) {
+        btn_prev.disabled = true;
+      } else {
+        btn_prev.disabled = false;
+      }
+}
+
+
+
+
+btn_prev.addEventListener("click",function(){
+		 pageNumberHere = pageNumberHere - 1;
+         getData();
+
+})
+
+
+btn_Next.addEventListener("click",function(){
+		 pageNumberHere = pageNumberHere  + 1;
+         getData();
+})
+
+getData();
+
+
